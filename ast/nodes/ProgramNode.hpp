1c46601a65a6ea8d43505a8a4ed99ed7a83cfe64
Marco-Brescia <marcbresc03@gmail.com>
2025-12-02 10:33:30 +0100

    Bug fix: added constructor for ProgramNode.hpp

--- ast/nodes/ProgramNode.hpp
+++ ast/nodes/ProgramNode.hpp
@@ -10,5 +10,8 @@ class ProgramNode: public ASTNode {
         std::vector<std::unique_ptr<ASTNode>> globals; // vettore di nodi per dichiarazioni globali
         std::unique_ptr<ASTNode> mainBlock;
 
+        ProgramNode(std::vector<std::unique_ptr<ASTNode>> globals, std::unique_ptr<ASTNode> mainBlock)
+            : globals(std::move(globals)), mainBlock(std::move(mainBlock)) {}
+
         void accept(ASTVisitor& visitor) override;
 };
