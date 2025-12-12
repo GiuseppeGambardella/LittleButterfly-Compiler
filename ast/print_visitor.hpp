b2c93bc170bdfd133b9cce901fa0b3d8a6870015
Marco-Brescia <marcbresc03@gmail.com>
2025-12-12 10:38:07 +0100

    Refactor print_visitor.hpp

--- ast/print_visitor.hpp
+++ ast/print_visitor.hpp
@@ -10,12 +10,10 @@
 
 class PrintVisitor : public ASTVisitor {
     public:
-        void visit(NumberNode& node) override {
-            std::cout << "Number: " << node.value << std::endl;
-        }
-
-        void visit(StringNode& node) override {
-            std::cout << "String: " << node.value << std::endl;
+        void visit(AssignmentNode& node) override {
+            std::cout << "Assignment to variable: " << node.variableName << std::endl;
+            std::cout << "Value:" << std::endl;
+            node.value->accept(*this);
         }
 
         void visit(BinaryOpNode& node) override {
@@ -26,19 +24,43 @@ class PrintVisitor : public ASTVisitor {
             node.right->accept(*this);
         }
 
-        void visit(VariableNode& node) override {
-            std::cout << "Variable: " << node.name << std::endl;
+        void visit(BlockNode& node) override {
+            std::cout << "Block:" << std::endl;
+            for (auto& stmt : node.statements) {
+                stmt->accept(*this);
+            }
         }
 
-        void visit(AssignmentNode& node) override {
-            std::cout << "Assignment to variable: " << node.variableName << std::endl;
-            std::cout << "Value:" << std::endl;
-            node.value->accept(*this);
+        void visit(BooleanNode &node) override {
+            std::cout << "Boolean: " << (node.value ? "true" : "false") << std::endl;
         }
 
-        void visit(PrintNode& node) override {
-            std::cout << "Print statement:" << std::endl;
-            node.expression->accept(*this);
+        void visit(CharNode& node) override {
+            std::cout << "Char: " << node.value << std::endl;
+        }
+
+        void visit(FunctionCallNode& node) override {
+            std::cout << "Function call: " << node.functionName << std::endl;
+            std::cout << "Arguments:" << std::endl;
+            for (auto& arg : node.arguments) {
+                arg->accept(*this);
+            }
+        }
+
+        void visit(FunctionDeclNode& node) override {
+            std::cout << "Function definition: " << node.name << std::endl;
+            std::cout << "Parameters: ";
+            for (auto& param : node.parameters) {
+                std::cout << param.name << " : ";
+                param.type.accept(*this);
+                std::cout << "; ";
+            }
+            std::cout << std::endl;
+            std::cout << "Return type:" << std::endl;
+            node.returnType->accept(*this);
+            std::cout << std::endl;
+            std::cout << "Body:" << std::endl;
+            node.body->accept(*this);
         }
 
         void visit(IfNode& node) override {
@@ -61,40 +83,13 @@ class PrintVisitor : public ASTVisitor {
             node.body->accept(*this);
         }
 
-        void visit(FunctionDeclNode& node) override {
-            std::cout << "Function definition: " << node.name << std::endl;
-            std::cout << "Parameters: ";
-            for (auto& param : node.parameters) {
-                std::cout << param.name << " : ";
-                param.type.accept(*this);
-                std::cout << "; ";
-            }
-            std::cout << std::endl;
-            std::cout << "Return type:" << std::endl;
-            node.returnType->accept(*this);
-            std::cout << std::endl;
-            std::cout << "Body:" << std::endl;
-            node.body->accept(*this);
+        void visit(NumberNode& node) override {
+            std::cout << "Number: " << node.value << std::endl;
         }
 
-        void visit(FunctionCallNode& node) override {
-            std::cout << "Function call: " << node.functionName << std::endl;
-            std::cout << "Arguments:" << std::endl;
-            for (auto& arg : node.arguments) {
-                arg->accept(*this);
-            }
-        }
-
-        void visit(ReturnNode& node) override {
-            std::cout << "Return statement:" << std::endl;
-            node.value->accept(*this);
-        }
-
-        void visit(BlockNode& node) override {
-            std::cout << "Block:" << std::endl;
-            for (auto& stmt : node.statements) {
-                stmt->accept(*this);
-            }
+        void visit(PrintNode& node) override {
+            std::cout << "Print statement:" << std::endl;
+            node.expression->accept(*this);
         }
 
         void visit(ProgramNode& node) override {
@@ -106,21 +101,6 @@ class PrintVisitor : public ASTVisitor {
             node.mainBlock->accept(*this);
         }
 
-
-        void visit(UnaryOpNode& node) override {
-            std::cout << "Unary Operation: " << node.op << std::endl;
-            std::cout << "Operand:" << std::endl;
-            node.operand->accept(*this);
-        }
-
-        void visit(TypeNode& node) override {
-            std::cout << "Type: " << typeToString(node.type) << std::endl; //Funzione per ritornare il tipo in Stringa dall'enum
-        }
-
-        void visit(CharNode& node) override {
-            std::cout << "Char: " << node.value << std::endl;
-        }
-
         void visit(ReadNode& node) override {
             std::cout << "Read statement for variable:" << std::endl;
             node.variable->accept(*this);
@@ -128,6 +108,25 @@ class PrintVisitor : public ASTVisitor {
 
         void visit(RealNode& node) override {
             std::cout << "Real: " << node.value << std::endl;
+        }
+
+        void visit(ReturnNode& node) override {
+            std::cout << "Return statement:" << std::endl;
+            node.value->accept(*this);
+        }
+
+        void visit(StringNode& node) override {
+            std::cout << "String: " << node.value << std::endl;
+        }
+
+        void visit(TypeNode& node) override {
+            std::cout << "Type: " << typeToString(node.type) << std::endl; //Funzione per ritornare il tipo in Stringa dall'enum
+        }
+
+        void visit(UnaryOpNode& node) override {
+            std::cout << "Unary Operation: " << node.op << std::endl;
+            std::cout << "Operand:" << std::endl;
+            node.operand->accept(*this);
         }
 
         void visit(VarDeclNode &node) override {
@@ -139,11 +138,11 @@ class PrintVisitor : public ASTVisitor {
             }
         }
 
-        void visit(VoidNode &node) override {
-            std::cout << "Void Node" << std::endl;
+        void visit(VariableNode& node) override {
+            std::cout << "Variable: " << node.name << std::endl;
         }
 
-        void visit(BooleanNode &node) override {
-            std::cout << "Boolean: " << (node.value ? "true" : "false") << std::endl;
+        void visit(VoidNode &node) override {
+            std::cout << "Void Node" << std::endl;
         }
 };
